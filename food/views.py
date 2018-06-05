70b83de571b0d3066344a76af0442ec86b44ebc0
cabub <cabub@cabub.me>
2018-06-04 21:07:18 -0500

    added endpoints for searching for nutritional facts

--- food/views.py
+++ food/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import os
+from food.nalclient import NALClient
 
+
+_client = NALClient(os.environ['NAL_API_KEY'])
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("What up")
+
+
+def search(request):
+    if 'q' not in request.GET:
+        return HttpResponse('q parameter is required', status=400)
+    return JsonResponse(_client.search(request.GET.get('q')))
+
